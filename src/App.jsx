a3096fb441e2d847f0d2e023d998e14210eec5df
Bright Kingsley <110265893+BrightKingsley@users.noreply.github.com>
2023-08-19 22:37:21 +0100

    Update App.jsx for redeployment

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,7 @@ import React from "react";
 import Card from "./components/UI/Card";
 import Container from "./components/container/Container";
 
-const App = () => {
-  return <Container />;
-};
+const App = () => <Container />;
+
 
 export default App;
